a07ae3ae7bbb1411e4200c6d9276168917207d78
jianghaiyusheng <958463867@qq.com>
2022-03-24 09:52:52 +0800

    下一步基于枚举重构代码

--- core/const.py
+++ core/const.py
@@ -1,3 +1,26 @@
+from enum import Enum, unique
+
+@unique
+class E(Enum):
+    def __new__(cls, sign: str, is_required: bool, is_view:bool, value:int):
+        obj = object.__new__(cls)
+        obj.sign = sign
+        obj.is_required = is_required
+        obj.is_view = is_view
+        obj._value_ = value
+        return obj
+    totalSupply = "totalSupply()", True, True, 1
+    balanceOf = "balanceOf(address)", True, True, 2
+    allowance = "allowance(address,address)", True, True, 3
+    transfer = "transfer(address,uint256)", True, False, 4
+    approve = "approve(address,uint256)", True, False, 5
+    transferFrom = "transferFrom(address,address,uint256)", True, False, 6
+    burn = "burn(uint256)", False, False, 7
+    increaseAllowance = "increaseAllowance(address,uint256)", False, False, 8
+    decreaseAllowance = "decreaseAllowance(address,uint256)", False, False, 9
+# 下一步基于枚举重构代码
+    
+
 TOTAL_SUPPLY = "totalSupply"
 BALANCE_OF = "balanceOf"
 ALLOWANCE = "allowance"
