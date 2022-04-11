808eff16412d2b6165b2636e0d53f20afd3a1646
jianghaiyusheng <958463867@qq.com>
2022-04-11 10:27:11 +0800

    add the control config whether checks

--- main.py
+++ main.py
@@ -21,23 +21,25 @@ if __name__ == "__main__":
     contract_dir = download_sourceCode(chain, token_address, token_name)
     os.chdir(contract_dir)
 
-    is_single = config.getboolean('check','is_single')
-    contract_name = config.get('check','contract_name')
-    if contract_name.strip() == "":
-        print("请配置合约名")
-    else:
-        contract_path = contract_name + '.sol'
-        if not is_single:
-            contract_path = 'contracts/' + contract_path
-        slither = Slither(contract_path)
-        for c in slither.contracts_derived:
-            if c.kind!="contract":
-                continue
-            erc20_check = Erc20Check(c)
-            erc20_check.check_close()
-            erc20_check.check_standard_func()
-            erc20_check.check_overflow()
-            erc20_check.check_sstore()
-            erc20_check.check_fake_recharge()
-            erc20_check.check_call_other_contract()
+    now_check = config.getboolean('check','now_check')
+    if now_check:
+        is_single = config.getboolean('check','is_single')
+        contract_name = config.get('check','contract_name')
+        if contract_name.strip() == "":
+            print("请配置合约名")
+        else:
+            contract_path = contract_name + '.sol'
+            if not is_single:
+                contract_path = 'contracts/' + contract_path
+            slither = Slither(contract_path)
+            for c in slither.contracts_derived:
+                if c.kind!="contract":
+                    continue
+                erc20_check = Erc20Check(c)
+                erc20_check.check_close()
+                erc20_check.check_standard_func()
+                erc20_check.check_overflow()
+                erc20_check.check_sstore()
+                erc20_check.check_fake_recharge()
+                erc20_check.check_call_other_contract()
     sys.exit()
